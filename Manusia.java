80af2abcf26168d5ed00278ee0e05e0d416484e4
mohazralfathurrazaq <114301881+mohazralfathurrazaq@users.noreply.github.com>
2022-10-17 20:43:28 +0700

    Add files via upload

---
+++ Manusia.java
@@ -0,0 +1,19 @@
+
+public class Manusia {
+    
+    public void setBiodata(String nama){
+        
+        System.out.println("Method setBiodata pertama dipanggil");
+    }
+    
+    public void setBiodata(String nama , int umur){
+        
+        System.out.println("Method setBiodata kedua dipanggil");
+    }
+    
+    public void setBiodata(String nama, int umur, String alamat){
+        
+        System.out.println("Method setBiodata ketiga dipanggil");
+    }
+    
+}
---
+++ TestManusia.java
@@ -0,0 +1,9 @@
+
+public class TestManusia {
+    
+    public static void main(String[] args){
+        Manusia man = new Manusia();
+        man.setBiodata("Azral", 21, "Sukabumi");
+    }
+    
+}
